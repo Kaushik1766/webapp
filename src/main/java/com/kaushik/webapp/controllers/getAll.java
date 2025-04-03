d0adb6e4151b706e464b9840151f000e39f73899
Kaushik Saha <kaushiksaha004@gmail.com>
2025-04-03 12:11:06 +0530

    get route

--- src/main/java/com/kaushik/webapp/controllers/getAll.java
+++ src/main/java/com/kaushik/webapp/controllers/getAll.java
@@ -2,13 +2,18 @@ package com.kaushik.webapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class Test {
-    @RequestMapping("/getAll")
-    public String test() {
-        return "Hello.html";
+import com.kaushik.webapp.models.Product;
+
+@RestController
+public class getAll {
+    @GetMapping("/getAll")
+    public Product test() {
+        Product product = new Product("a", "asdf", 1232.3);
+
+        return product;
     }
 }
---
+++ src/main/java/com/kaushik/webapp/models/Product.java
@@ -0,0 +1,16 @@
+package com.kaushik.webapp.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private String name;
+    private String description;
+    private double price;
+}
